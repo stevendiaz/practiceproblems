4ace2e92a0ad52aad251c466be12d45afa54ce72
stevendiaz <stevendiaz@utexas.edu>
2015-12-30 23:24:19 -0600

    1.7 works for more than one zero

--- problemsolutions/ctci/c1p7.java
+++ problemsolutions/ctci/c1p7.java
@@ -27,16 +27,24 @@ public class c1p7 {
 		}
 	}
 	
-	private static int[][] setZeros(int[][] arr) {		
+	private static int[][] setZeros(int[][] arr) {
+		boolean[][] zeroFinder = new boolean[arr.length][arr[0].length];
+		//first pass, mark all 0s as true
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[0].length; j++) {
+				if(arr[i][j] == 0)
+					zeroFinder[i][j] = true;
+			}
+		}
+		//second pass, modify all the trues
 		for(int r = 0; r < arr.length; r++) {
 			for(int c = 0; c < arr[0].length; c++) {
-				if(arr[r][c] == 0) {
+				if(zeroFinder[r][c]) {
 					//loop through the entire column
 					for(int i = 0; i < arr[0].length; i++)
 						arr[r][i] = 0;
 					for(int j = 0; j < arr.length; j++) 
 						arr[j][c] = 0;
-					return arr;
 				}
 			}
 		}		
