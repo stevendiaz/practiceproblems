7180b58a39bb7885e177358034c04ac4abfe0861
Steven Diaz <stevendiaz@utexas.edu>
2016-01-01 18:34:01 -0600

    White space and code deletion

--- problemsolutions/ctci/c1p15.java
+++ problemsolutions/ctci/c1p15.java
@@ -7,14 +7,13 @@ public class c1p15 {
 		System.out.println("pales, pale -> " +oneAway("pales","pale"));
 		System.out.println("pale, bale -> " +oneAway("pale", "bale"));
 		System.out.println("pale, bake -> " +oneAway("pale", "bake"));
-
 	}
 	
 	public static boolean oneAway(String original, String changed) {
 		boolean result = false;
+		//Count number of characters
 		int[] originalCount = new int[256];
 		int[] changedCount = new int[256];
-		//Count characters
 		for(int i = 0; i < original.length(); i++)
 			originalCount[(int)original.charAt(i)]++;
 		for(int j = 0; j < changed.length(); j++)
@@ -42,18 +41,5 @@ public class c1p15 {
 		}		
 		return result;
 	}
-}private static int[][] setZeros(int[][] arr) {		
-		for(int r = 0; r < arr.length; r++) {
-			for(int c = 0; c < arr[0].length; c++) {
-				if(arr[r][c] == 0) {
-					//loop through the entire column
-					for(int i = 0; i < arr[0].length; i++)
-						arr[r][i] = 0;
-					for(int j = 0; j < arr.length; j++) 
-						arr[j][c] = 0;
-					return arr;
-				}
-			}
-		}		
-		return arr;
+}
 	
