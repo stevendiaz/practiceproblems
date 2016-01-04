24ae34a00024c1b788cbbb6018edf4a6029b8229
Steven Diaz <stevendiaz@utexas.edu>
2016-01-04 01:13:47 -0600

    Update c1p5.java

--- problemsolutions/ctci/c1p5.java
+++ problemsolutions/ctci/c1p5.java
@@ -1,5 +1,6 @@
 public class c1p5 {
 	public static String replaceSpace(String input) {
+		//Potential rework without appending function?
 		StringBuilder result = new StringBuilder();
 		for(int i = 0; i < input.length(); i++) {
 			if(input.charAt(i) == ' ') {
